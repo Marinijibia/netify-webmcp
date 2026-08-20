999acd005ac74a7bb23b30f369cbf4e8fb249ded
marinijibia <marinijibiaa@gmail.com>
2026-08-20 18:35:37 +0100

    fix(metro): retain unstable_enablePackageExports for modern ESM resolution

--- Netify/metro.config.js
+++ Netify/metro.config.js
@@ -4,7 +4,4 @@ const { withNativewind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Enable modern package exports resolution for TanStack Query and ESM modules
-config.resolver.unstable_enablePackageExports = true;
-
 module.exports = withNativewind(config);
